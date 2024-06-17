403f754b866fbd05ee02db99993831d3b2a3ebe4
oyindamola12 <mshittu234@gmail.com>
2024-06-18 00:24:35 +0100

    initial commit

---
+++ Public/script.js
@@ -0,0 +1,189 @@
+
+function openExclusive() {
+
+document.getElementById("overlayExclusive").style.display = "block";
+
+ //open the paystack's payment modal
+}
+function closeExclusive() {
+
+document.getElementById("overlayExclusive").style.display = "none";
+
+ //open the paystack's payment modal
+}
+function openExclusive2() {
+var phoneNo = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+
+fetch('https://www.mpageshub.com/exclusive', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+ body: JSON.stringify({
+ email:email,
+ amount:phoneNo,
+
+
+
+})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Server response:', data);
+            // You can handle the server response as needed
+        })
+        .catch(error => {
+            console.error('Error sending data to server:', error);
+        });
+
+
+    var handler = PaystackPop.setup({
+        key: 'pk_live_8db47ccef2cfc6bc1148849f867225a5de373772', //put your public key here
+        email:  email, //put your customer's email here
+        amount:8000*100, //amount the customer is supposed to pay
+        metadata: {
+            custom_fields: [
+                {
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+
+                }
+            ]
+        },
+        callback: function (response) {
+           if (response.status){
+          document.getElementById("centerExclusive").style.display = "block";
+          document.getElementById("overlayExclusive").style.display = "none";
+
+           }
+
+
+
+
+        },
+        onClose: function () {
+            //when the user close the payment modal
+            alert('Transaction cancelled');
+        }
+    });
+    handler.openIframe(); //open the paystack's payment modal
+
+
+ //open the paystack's payment modal
+}
+
+function openStandard() {
+
+document.getElementById("overlayStandard").style.display = "block";
+
+ //open the paystack's payment modal
+}
+
+function closeStandard() {
+
+document.getElementById("overlayStandard").style.display = "none";
+
+ //open the paystack's payment modal
+}
+
+function openStandard2() {
+var phoneNo = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+
+fetch('https://www.mpageshub.com/standard', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+ body: JSON.stringify({
+ email:email,
+ amount:phoneNo,
+
+
+
+})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Server response:', data);
+            // You can handle the server response as needed
+        })
+        .catch(error => {
+            console.error('Error sending data to server:', error);
+        });
+
+
+    var handler = PaystackPop.setup({
+        key: 'pk_live_8db47ccef2cfc6bc1148849f867225a5de373772', //put your public key here
+        email:  email, //put your customer's email here
+        amount:4000*100, //amount the customer is supposed to pay
+        metadata: {
+            custom_fields: [
+                {
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+
+                }
+            ]
+        },
+        callback: function (response) {
+           if (response.status){
+          document.getElementById("centerStandard").style.display = "block";
+          document.getElementById("overlayStandard").style.display = "none";
+
+           }
+
+
+
+
+        },
+        onClose: function () {
+            //when the user close the payment modal
+            alert('Transaction cancelled');
+        }
+    });
+    handler.openIframe(); //open the paystack's payment modal
+
+
+ //open the paystack's payment modal
+}
+
+function openBasic() {
+ document.getElementById("overlayBasic").style.display = "block";
+ //open the paystack's payment modal
+}
+
+function closeBasic() {
+ document.getElementById("overlayBasic").style.display = "none";
+ //open the paystack's payment modal
+}
+function openBasic2() {
+
+var phoneNo = document.getElementById('phone').value;
+  var email = document.getElementById('email').value;
+
+    fetch('https://www.mpageshub.com/basics', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+ body:JSON.stringify({
+ phoneNo:phoneNo,
+ email: email,
+
+
+  })
+           })
+        .then(response =>  response.json())
+        .then( async data => {
+          document.getElementById("centerBasic").style.display = "block";
+          document.getElementById("overlayBasic").style.display = "none";
+
+        })
+        .catch(error => {
+            console.error('Error sending data to server:', error);
+        });
+
+ //open the paystack's payment modal
+}
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const bodyparser = require('body-parser');
+const admin = require('firebase-admin');
+const path =require('path')
+const bodyParser = require('body-parser');
+const { initializeApp} = require( "firebase/app");
+const publicPath=path.join(__dirname,'Public')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(publicPath))
+
+app.use(express.json({ limit: "1000mb", extended: true }));
+app.use(express.urlencoded({  limit: "1000mb", extended: true, parameterLimit: 50000}));
+
+const serviceAccount = require('./dynasty-18f4b-firebase-adminsdk-bgho6-7138e0b250.json');
+app.set('view engine', 'ejs');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // databaseURL: "https://mpages-6ed7a-default-rtdb.firebaseio.com",
+
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeO26540I6nx59K7252zo3-TQ-Wp9CMuk",
+  authDomain: "dynasty-18f4b.firebaseapp.com",
+  projectId: "dynasty-18f4b",
+  storageBucket: "dynasty-18f4b.appspot.com",
+  messagingSenderId: "879185725666",
+  appId: "1:879185725666:web:e7ec9ab6802d0d499436bb"
+};
+
+initializeApp(firebaseConfig);
+
+app.post('/basic',async (req, res)=> {
+    const phoneNo= req.body.phoneNo;
+
+const email = req.body.email;
+
+const businessDb =  db.collection('Basic');
+await businessDb.add({
+phoneNo:phoneNo,
+email:email,
+
+});
+});
+
+app.post('/standard',async (req, res)=> {
+    const phoneNo= req.body.phoneNo;
+
+const email = req.body.email;
+
+const businessDb =  db.collection('Standard');
+await businessDb.add({
+phoneNo:phoneNo,
+email:email,
+
+});
+});
+
+app.post('/exclusive',async (req, res)=> {
+    const phoneNo= req.body.phoneNo;
+
+const email = req.body.email;
+
+const businessDb =  db.collection('Exclusive');
+await businessDb.add({
+phoneNo:phoneNo,
+email:email,
+
+});
+});
+app.listen(PORT, ()=> console.log('App is listening on url http://localhost:' + PORT))
